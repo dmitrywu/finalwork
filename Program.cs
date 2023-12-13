c358d2ad97b867cd48967c301d0efa02dd6378ac
Dmitry <dmitry.wu@gmail.com>
2023-12-13 18:36:48 +0300

    magic number 3 del. final???

--- Program.cs
+++ Program.cs
@@ -3,20 +3,20 @@
 Первоначальный массив можно ввести с клавиатуры, либо ***задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. */
 
-static int OutArraySizeCalculate(string[] array)
+static int OutArraySizeCalculate(string[] array, int wordSize)
 {
     int count = 0;
-    for (int i = 0; i < array.Length; i++) if (array[i].Length <= 3) count++;
+    for (int i = 0; i < array.Length; i++) if (array[i].Length <= wordSize) count++;
     return count;
 }
 
-static string[] OutArrayForming(string[] inputArray, int outArraySize)
+static string[] OutArrayForming(string[] inputArray, int outArraySize, int wordSize)
 {
-    string[] outputArray = new string[OutArraySizeCalculate(inputArray)];
+    string[] outputArray = new string[OutArraySizeCalculate(inputArray, wordSize)];
     int count = outArraySize;
 
     for (int i = 0; i < inputArray.Length; i++)
-        if (inputArray[i].Length <= 3)
+        if (inputArray[i].Length <= wordSize)
         {
             outputArray[outputArray.Length - count] = inputArray[i];
             count--;
@@ -24,7 +24,8 @@ static string[] OutArrayForming(string[] inputArray, int outArraySize)
     return outputArray;
 }
 
-string[] inputArray = new string[] { "all", "you", "need", "is", "mnogo", "ochen", ",", "mnogo", "deneg", "and", "other", "stuff", "!" };
+string[] inputArray = new string[] { "all", "you", "need", "is", "mnogo", "ochen", ",", "mnogo", "deneg", "and", "other", "stuff", "!", "AAAaaa!", "need", "more tests" };
+int maxWordSize = 3;
 
 Console.WriteLine($"IN: Length: {inputArray.Length}, Content: {string.Join(" ", inputArray)}");
-Console.WriteLine($"OUT: Length: {OutArraySizeCalculate(inputArray)}, Content: {string.Join(" ", OutArrayForming(inputArray, OutArraySizeCalculate(inputArray)))}");
+Console.WriteLine($"OUT: Length: {OutArraySizeCalculate(inputArray, maxWordSize)}, Content: {string.Join(" ", OutArrayForming(inputArray, OutArraySizeCalculate(inputArray, maxWordSize), maxWordSize))}");
